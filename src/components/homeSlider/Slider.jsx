7b7587efd28e735c28134ac0bd838201c12a833e
Navin <nnshri2000@gmail.com>
2024-07-12 20:30:35 +0530

    Describe the changes you made

--- src/components/homeSlider/Slider.jsx
+++ src/components/homeSlider/Slider.jsx
@@ -6,11 +6,10 @@ const Slider = () => {
     <div className="slider" id="home">
       <video autoplay="autoplay" loop playsInline muted>
         <source src="/src/assets/logo-vid.webp" type="video/webm" />
-        <source src="/src/assets/logo-vid.mp4" type="video/mp4" />
+        <source src="https://res.cloudinary.com/dajpvfbfm/video/upload/v1720792041/logo-vid_ggjrzq.mp4" type="video/mp4" />
         Your browser does not support the video tag.
       </video>
     </div>
   );
 };
-
 export default Slider
